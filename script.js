8be800468a318ff73da122e4f8b8b3fd15347f5e
Taynara Pecorario <taynara_rp22@hotmail.com>
2023-01-13 20:19:51 -0300

    :sparkles: adding close cards and finish functions

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 let quantityCards = 0;
+let quantityOfClicks = 0;
+let time = 0;
+
+let listOfGifs = [];
 let turnedCards = [];
+let listOfRightCards = [];
+
+let cardSelected;
+
+const data = [
+  'bobrossparrot.gif',
+  'explodyparrot.gif',
+  'fiestaparrot.gif',
+  'metalparrot.gif',
+  'revertitparrot.gif',
+  'tripletsparrot.gif',
+  'unicornparrot.gif'
+];
+
+function comparator() {
+  return Math.random() - 0.5;
+}
+
+// function addTime() {
+//   time++;
+// }
+
+function clearGame() {
+  quantityCards = 0;
+  turnedCards = [];
+  listOfGifs = [];
+  quantityOfClicks = 0;
+  cardSelected = '';
+  listOfRightCards = [];
+
+  const list = document.querySelector('ul');
+
+  list.innerHTML = '';
+}
 
 function getQuantityOfCards() {
   while (quantityCards % 2 !== 0 || quantityCards < 4 || quantityCards > 14) {
@@ -11,8 +49,22 @@ function getQuantityOfCards() {
   createGame();
 }
 
+function getGifs() {
+  data.sort(comparator);
+
+  for (let i = 0; i < quantityCards / 2; i++) {
+    const item = data[i];
+    listOfGifs.push(item);
+    listOfGifs.push(item);
+  }
+
+  listOfGifs.sort(comparator);
+}
+
 function createGame() {
   const list = document.querySelector('ul');
+
+  getGifs();
 
   for (let i = 0; i < quantityCards; i++) {
     list.innerHTML += `<li class="card" onclick="handleTurnCard(this, ${i})">
@@ -20,7 +72,7 @@ function createGame() {
       <img src="assets/back.png" alt="" />
     </div>
     <div class="face opened">
-      <img src="assets/bobrossparrot.gif" alt="" />
+      <img src="assets/${listOfGifs[i]}" alt="" />
     </div>
   </li>`;
   }
@@ -32,27 +84,52 @@ function handleTurnAllCardsBack() {
   const cardTwo = list.children[turnedCards[1]];
 
   setTimeout(() => {
-    cardOne.children[0].style.transform = 'rotateY(0deg)';
-    cardOne.children[1].style.transform = 'rotateY(-180deg)';
+    cardOne.querySelector('.closed img').style.transform = 'rotateY(0deg)';
+    cardOne.querySelector('.opened img').style.transform = 'rotateY(-180deg)';
 
-    cardTwo.children[0].style.transform = 'rotateY(0deg)';
-    cardTwo.children[1].style.transform = 'rotateY(-180deg)';
+    cardTwo.querySelector('.closed img').style.transform = 'rotateY(0deg)';
+    cardTwo.querySelector('.opened img').style.transform = 'rotateY(-180deg)';
   }, '1000');
 
   turnedCards = [];
+}
+
+function handleAddRightCards() {
+  listOfRightCards = [...listOfRightCards, ...turnedCards];
+
+  turnedCards = [];
+
+  if (listOfRightCards.length === Number(quantityCards)) {
+    setTimeout(() => {
+      alert(`Você ganhou em ${quantityOfClicks} jogadas!`);
+      clearGame();
+    }, 500);
+  }
 }
 
 function handleTurnCard(card, index) {
   const back = card.querySelector('.closed');
   const opened = card.querySelector('.opened');
 
-  opened.style.transform = 'rotateY(0deg)';
-  back.style.transform = 'rotateY(-180deg)';
+  if (!listOfRightCards.includes(index)) {
+    quantityOfClicks++;
+    opened.style.transform = 'rotateY(0deg)';
+    back.style.transform = 'rotateY(-180deg)';
 
-  turnedCards.push(index);
+    turnedCards.push(index);
 
-  if (turnedCards.length === 2) {
-    handleTurnAllCardsBack();
+    if (turnedCards.length === 1) {
+      cardSelected = card;
+    } else if (turnedCards.length === 2) {
+      const currentCard = card.querySelector('.opened img').getAttribute('src');
+      const selectedCard = cardSelected
+        .querySelector('.opened img')
+        .getAttribute('src');
+
+      currentCard === selectedCard
+        ? handleAddRightCards()
+        : handleTurnAllCardsBack();
+    }
   }
 }
 
